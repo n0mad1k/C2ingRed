ee1c077ab1d8fc16a22aad846b56f2bdf0e4dd5d
n0mad1k <n0m4d1k@protonmail.com>
2025-04-10 10:29:19 -0400

    Reverting to what worked/splitting to new tool

--- FlokiNET/files/serve-beacons.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Simple HTTP server to serve beacon files
-
-cd /opt/c2/implants
-# Copy beacons to more generic names
-cp $(ls *windows*.exe | head -1) win-beacon.exe
-cp $(ls *linux* | grep -v "exe" | head -1) linux-beacon
-python3 -m http.server 8080 --bind 127.0.0.1
